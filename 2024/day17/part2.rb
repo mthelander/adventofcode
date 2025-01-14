e521617fe3fe59b4d042e6b272b12c351bd757d3
Michael Thelander <mjthelander@gmail.com>
2025-01-13 21:35:04 -0800

    cleanup part 2

--- 2024/day17/part2.rb
+++ 2024/day17/part2.rb
@@ -1,23 +1,19 @@
 def test(a, i)
-  (i ^ 2) ^ ((a >> (i ^ 2)) ^ 3)
+  (i ^ 2) ^ ((a >> (i ^ 2)) ^ 3) % 8
 end
 
 def search(out, x=0)
-  return [x] if out.empty?
+  return x if out.empty?
   x <<= 3
   z = out[-1]
-  vals = []
   8.times do |i|
     a = x ^ i
-    r = test(a, i)
-    if r % 8 == z
+    if z == test(a, i)
       v = search(out[0..-2], a)
-      unless v.empty?
-        return vals + v
-      end
+      return v unless v.nil?
     end
   end
-  return vals
+  return nil
 end
 
 p search([2,4,1,2,7,5,1,3,4,3,5,5,0,3,3,0])
